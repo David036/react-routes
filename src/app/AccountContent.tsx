d775b6b795ed2a06a1300ce3102fe448c826f1f7
David036 <hovhannisyandav@icloud.com>
2022-07-05 03:05:32 -0700

    Fixed minor comment

--- src/app/AccountContent.tsx
+++ src/app/AccountContent.tsx
@@ -5,7 +5,7 @@ export default function AccountContent(): JSX.Element {
 
   return (
     <div className="account">
-      <h1>Account {id}</h1>
+      <h1>Account {id === "1" ? "one" : id === "2" ? "two" : "three"}</h1>
       <NavLink to="/">Back to home</NavLink>
     </div>
   );
